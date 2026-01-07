cc8754542ef9347a2847a346870e94aaf5491fa2
gilakopiabis-afk <gilakopiabis@gmail.com>
2026-01-07 15:15:51 +0700

    Update bot.py

--- bot.py
+++ bot.py
@@ -2,8 +2,13 @@ import os
 import json
 from google.oauth2.service_account import Credentials
 
-creds_json = json.loads(os.environ["GOOGLE_CREDENTIALS"])
+SCOPES = ["https://www.googleapis.com/auth/spreadsheets"]
+
+service_account_info = json.loads(
+    os.environ.get("GOOGLE_CREDENTIALS_JSON")
+)
+
 creds = Credentials.from_service_account_info(
-    creds_json,
-    scopes=["https://www.googleapis.com/auth/spreadsheets"]
+    service_account_info,
+    scopes=SCOPES
 )
